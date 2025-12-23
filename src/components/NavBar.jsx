7e3e0c1c0f6fe229d3a92de77373a93edbb30791
smkakitani <82690252+kakisaura@users.noreply.github.com>
2025-12-23 01:55:59 -0300

    change NavLink to Link, use authentication to display different links to user

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,14 @@
 // Styles
 
 // Router
-import { NavLink } from "react-router";
+import { Link } from "react-router";
+import { useAuth } from "../utils/AuthContext";
 
 
 
 // 
 function NavBar() {
+  const { token, user } = useAuth();
   const menuList = [
     "home",
     "posts",
@@ -17,13 +19,22 @@ function NavBar() {
   return (
     <nav>
       <menu>
-        {menuList.map((item) => (
-          <li key={item}>
-            <NavLink to={item === "home" ? "/" : item}>
-            {item}
-            </NavLink>
-          </li>
-        ))}
+        <li>
+          <Link to="/">{menuList[0]}</Link>
+        </li>
+        <li>
+          <Link to={menuList[1]}>{menuList[1]}</Link>
+        </li>
+        {/* Shows current user account if logged in */}
+        {!token ? <li>
+          <Link to={menuList[2]}>{menuList[2]}</Link>
+        </li> : <li>
+          <Link to={"user/" + user}>hi, {user}</Link>
+        </li>}
+        {/* Shows sign up if there's no user */}
+        {!token && <li>
+          <Link to={menuList[3]}>{menuList[3]}</Link>
+        </li>}
       </menu>
     </nav>
   );
