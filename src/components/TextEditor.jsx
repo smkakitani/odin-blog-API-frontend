82de2461e2fcea74438ea226a7155b0405ebb41b
smkakitani <82690252+kakisaura@users.noreply.github.com>
2025-12-23 01:45:16 -0300

    create component to setup TinyMCE text editor

---
+++ src/components/TextEditor.jsx
@@ -0,0 +1,41 @@
+// Styles
+
+// React
+// TinyMCE - rich text editor
+import { Editor } from "@tinymce/tinymce-react";
+
+
+
+// 
+export default function TextEditor({ ref, onDirty, }) {
+
+  return (
+    <>
+      <Editor
+        apiKey="2fgyetumwkdur5imzv6l7o9p985jfehh5dn447ur9b9kirx2" // My API
+        onInit={ (_evt, editor) => ref.current = editor }
+        onDirty={onDirty}
+        ref={ref}
+
+        init={{ 
+          width: 800,
+          height: 500,
+          resize: 'both',
+          menubar: true,
+          plugins: [ // Open-source plugins!
+            'code', 'codesample', 'anchor', 'autolink', 'link', 'lists', 'advlist', 'charmap', 'emoticons', 'wordcount', 'preview',
+          ],
+          toolbar_mode: 'wrap', 
+          // toolbar_sticky: true,
+          toolbar: 'undo redo | styles | ' +
+            'link | bold italic forecolor backcolor fontfamily fontsize lineheight | alignleft aligncenter ' + 'alignright alignjustify | bullist numlist outdent indent | ' + 'code codesample | charmap emoticons' +
+            'removeformat | print',
+          // toolbar1: 'undo redo | styles | bold italic underline | forecolor | link image | bullist numlist',
+          // toolbar2: 'outdent indent | alignleft aligncenter alignright',
+          // charmap: [[0x2615, 'morning coffee']],
+          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+        }}
+      />
+    </>
+  );
+}
