b2d756e66ad7c815484c13ef687666ca04026be1
smkakitani <82690252+kakisaura@users.noreply.github.com>
2026-01-02 15:49:55 -0300

    add more options

--- src/utils/lib.jsx
+++ src/utils/lib.jsx
@@ -1,11 +1,34 @@
-
-
-
 // 
-function prettifyDate(date) {
-  // undefined - it means that locale will depend on user's system setting
-  const prettyDate = new Date(date).toLocaleString(undefined, { hourCycle: "h23" })
-  return prettyDate;
+function prettifyDate(date, options) {
+  const opt = {
+    hourCycle: "h23",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }
+
+
+  
+  /* 
+    undefined - it means that locale will depend on user's system setting
+  */
+  switch (options) {
+    case "date":
+      /* 
+        Date format: 01/30/2000 -> and depending on user's location
+      */
+      return new Date(date).toLocaleString(undefined, { hourCycle: "h23" });
+    case "fullDate":
+      /* 
+        Full date format: day ("9"),month ("December"), 2000 -> and depending on user's location 
+      */
+      return new Date(date).toLocaleDateString(undefined, opt/* { hourCycle: "h23" } */);
+    default:
+      /* 
+        "date" as default
+      */
+      return new Date(date).toLocaleString(undefined, { hourCycle: "h23" });
+  }
 }
 
 export {
