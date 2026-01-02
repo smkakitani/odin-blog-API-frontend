4af5e11a90e94aad3482939cd74528a5d3a14520
smkakitani <82690252+kakisaura@users.noreply.github.com>
2026-01-02 16:03:41 -0300

    remove some comments

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -1,28 +1,28 @@
 // Styles
 
-// React
 // TinyMCE - rich text editor
 import { Editor } from "@tinymce/tinymce-react";
 
 
 
 // 
-export default function TextEditor({ ref, onDirty, }) {
+export default function TextEditor({ ref, onDirty, initialValue }) {
 
   return (
     <>
       <Editor
-        apiKey="2fgyetumwkdur5imzv6l7o9p985jfehh5dn447ur9b9kirx2" // My API
-        onInit={ (_evt, editor) => ref.current = editor }
+        apiKey="2fgyetumwkdur5imzv6l7o9p985jfehh5dn447ur9b9kirx2"
+        onInit={(_evt, editor) => ref.current = editor}
         onDirty={onDirty}
         ref={ref}
+        initialValue={initialValue}
 
         init={{ 
           width: 800,
           height: 500,
           resize: 'both',
           menubar: true,
-          plugins: [ // Open-source plugins!
+          plugins: [ /* Open-source plugins! */
             'code', 'codesample', 'anchor', 'autolink', 'link', 'lists', 'advlist', 'charmap', 'emoticons', 'wordcount', 'preview',
           ],
           toolbar_mode: 'wrap', 
@@ -30,9 +30,6 @@ export default function TextEditor({ ref, onDirty, }) {
           toolbar: 'undo redo | styles | ' +
             'link | bold italic forecolor backcolor fontfamily fontsize lineheight | alignleft aligncenter ' + 'alignright alignjustify | bullist numlist outdent indent | ' + 'code codesample | charmap emoticons' +
             'removeformat | print',
-          // toolbar1: 'undo redo | styles | bold italic underline | forecolor | link image | bullist numlist',
-          // toolbar2: 'outdent indent | alignleft aligncenter alignright',
-          // charmap: [[0x2615, 'morning coffee']],
           content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
         }}
       />
