b2e5b2f14955a28ff117ec8f7afa16a513a4d7ae
smkakitani <82690252+kakisaura@users.noreply.github.com>
2026-01-21 14:58:02 -0300

    add style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 // Styles
-
+import styled from "styled-components";
 // Router
 import { Link } from "react-router";
 import { useAuth } from "../utils/AuthContext";
@@ -7,6 +7,47 @@ import { useAuth } from "../utils/AuthContext";
 
 
 // 
+const NavStyle = styled.nav`
+  width: stretch;
+
+  margin-top: 10vh;
+
+  background-color: #000000;
+  border: 1px solid white;
+`;
+const UlStyle = styled.ul`
+  display: flex;
+  /* justify-content: space-evenly; */
+  gap: 1rem;
+
+  list-style: none;
+  font-family: "Doto";
+  font-size: 1.5rem;
+  font-weight: bolder;
+
+  text-decoration: none;
+
+  li {
+    padding: 0 0.5rem;
+
+    text-shadow: #8a2be2 0px 0px 5px
+    , blueviolet 0px 0px 7px
+    , blueviolet 0px 0px 10px
+    , #FF2D95 0px 0px 10px
+    , #FF2D95 0px 0px 15px
+    /* , #FF2D95 0px 0px 40px
+    , #FF2D95 0px 0px 50px
+    , #FF2D95 0px 0px 75px; */
+  }
+`;
+const StyledLink = styled(Link)`
+  color: blueviolet;
+
+  &:hover {
+    color: red;
+  }
+`;
+
 function NavBar() {
   const { token, user } = useAuth();
   const menuList = [
@@ -17,26 +58,28 @@ function NavBar() {
   ];
 
   return (
-    <nav>
-      <menu>
+    <NavStyle>
+      <UlStyle>
         <li>
-          <Link to="/">{menuList[0]}</Link>
+          <StyledLink to="/">{menuList[0]}</StyledLink>
         </li>
+        |
         <li>
-          <Link to={menuList[1]}>{menuList[1]}</Link>
+          <StyledLink to={menuList[1]}>{menuList[1]}</StyledLink>
         </li>
+        |
         {/* Shows current user account if logged in */}
         {!token ? <li>
-          <Link to={menuList[2]}>{menuList[2]}</Link>
+          <StyledLink to={menuList[2]}>{menuList[2]}</StyledLink>
         </li> : <li>
-          <Link to={user.type + "/" + user.username}>hi, {user.username}</Link>
-        </li>}
+          <StyledLink to={user.type + "/" + user.username}>hi, {user.username}</StyledLink>
+        </li>}|
         {/* Shows sign up if there's no user */}
         {!token && <li>
-          <Link to={menuList[3]}>{menuList[3]}</Link>
+          <StyledLink to={menuList[3]}>{menuList[3]}</StyledLink>
         </li>}
-      </menu>
-    </nav>
+      </UlStyle>
+    </NavStyle>
   );
 }
 
