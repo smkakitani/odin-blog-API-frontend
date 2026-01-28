c26f8f7baa63bfbd49489a663199eb72c175d22a
smkakitani <82690252+kakisaura@users.noreply.github.com>
2026-01-28 17:30:18 -0300

    add basic style

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,31 +1,37 @@
-// Style
+// Styles
 import '../styles/App.css';
+import styled from 'styled-components';
 // React
-import { useState } from 'react';
+// import { useState } from 'react';
 // Router
-import { Outlet, useParams } from 'react-router';
+import { Outlet } from 'react-router';
 // Components
 import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
 import { AuthProvider } from '../utils/AuthContext'; // Authentication logic with useContext and custom hook
 
 
 
-// When to ask for token on server? 
-/* 
-- user comment
-- user updating their data
-- user deleting their comment
- */
+// 
+const Main = styled.main`
+  color: rgb(144, 119, 235);
+  background-color: rgba(108, 33, 179, 0.8);
+  border-radius: 0.4em;
 
+  padding: 1rem;
+
+  min-height: 50vh;
+`;
 
 function App() {
-  const { postId } = useParams();
 
   return (
     <AuthProvider >
       <NavBar />
-      <p>Hi</p>
-      <Outlet />
+      <Main>
+        <Outlet />
+      </Main>
+      <Footer />
     </AuthProvider>
   )
 }
